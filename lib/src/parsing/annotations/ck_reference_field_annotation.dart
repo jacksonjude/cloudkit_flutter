81b373f082b43e07ba3c4aa88350101a79ea11aa
jacksonjude <jacksonjude@mac.com>
2022-07-19 22:48:05 -0700

    Using generic CKReferenceFieldAnnotation<T> to generate references

--- lib/src/parsing/annotations/ck_reference_field_annotation.dart
+++ lib/src/parsing/annotations/ck_reference_field_annotation.dart
@@ -4,23 +4,10 @@ import '../types/ck_reference.dart';
 import '../../api/request_models/ck_zone.dart';
 
 /// An annotation to link a local model class field to a CloudKit reference record field.
-class CKReferenceFieldAnnotation extends CKFieldAnnotation
+class CKReferenceFieldAnnotation<T> extends CKFieldAnnotation
 {
-  final CKReferenceGenerator _generator;
+  const CKReferenceFieldAnnotation(String name) : super(name);
 
-  const CKReferenceFieldAnnotation(String name, this._generator) : super(name);
-
-  CKReference createReference(String referenceUUID, {CKDatabase? database, CKZone? zone}) =>
-    _generator.createReference(referenceUUID, database: database, zone: zone);
-}
-
-class CKReferenceGenerator<T>
-{
-  /// The database for the reference generator.
-  final CKDatabase? _database;
-
-  const CKReferenceGenerator([this._database]);
-
-  CKReference createReference(String referenceUUID, {CKDatabase? database, CKZone? zone}) =>
-    CKReference<T>(referenceUUID, this._database ?? database!, zoneID: zone);
+  CKReference createReference(String referenceUUID, CKDatabase database, {CKZone? zone}) =>
+      CKReference<T>(referenceUUID, database, zoneID: zone);
 }
